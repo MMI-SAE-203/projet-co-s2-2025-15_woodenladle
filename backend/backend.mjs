093c02fd1670d5563404c1305ae04f48d57da1ba
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2025-04-15 16:36:06 +0200

    feat-Card

---
+++ backend/backend.mjs
@@ -0,0 +1,18 @@
+import PocketBase from "pocketbase";
+const pb = new PocketBase("http://127.0.0.1:8090");
+
+export async function allProduitSorted() {
+    try {
+        let record = await pb.collection('Produit').getFullList({
+            sort: 'Nom',
+        });
+        record = record.map((prod) => {
+            prod.img = pb.files.getURL(prod, prod.Image);
+            return prod;
+        });
+        return record;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des produits', error);
+        return [];
+    }
+}
