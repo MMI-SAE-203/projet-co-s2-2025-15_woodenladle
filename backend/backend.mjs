73f48552951faecf9b1e00ec34f482902e622c43
Mathis <mathis.guellati@edu.univ-fcomte.fr>
2025-05-14 17:28:52 +0200

    avatar header

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -98,5 +98,3 @@ export async function allEtapeByRecette(id) {
     });
     return record;
 }
-
-export async function user
--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from 'astro';
 import PocketBase from 'pocketbase';
 
+
 const pb = new PocketBase('http://127.0.0.1:8090');
 
 export const POST: APIRoute = async ({ request }) => {
