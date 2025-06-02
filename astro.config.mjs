62c6793f7094489ed5276fd2ee18190d1cee482f
Mathis <mathis.guellati@edu.univ-fcomte.fr>
2025-06-02 21:50:12 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    adapter: netlify(),
     output: 'server',
+    experimental: {
+        session: true, // ← active le flag expérimental si besoin
+    },
     vite: {
         plugins: [tailwindcss()]
     },
